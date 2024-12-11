e21485dfec53fc18d2bd1a32ccd85d7d20586388
Feytox <73577753+feytox@users.noreply.github.com>
2024-12-11 21:47:41 +0500

    fix: github link

--- quartz/quartz.layout.ts
+++ quartz/quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/feytox/fiit_summaries",
+      GitHub: "https://github.com/feytox/fiit-summaries",
       "ФИИТ": "https://fiit-urfu.ru",
     },
   }),
