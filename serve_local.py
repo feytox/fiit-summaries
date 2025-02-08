5eb694c6fae3afccc9ff765a753ac0a8af263ef2
Feytox <73577753+feytox@users.noreply.github.com>
2025-02-08 14:18:17 +0500

    feature: setup in serve_local

--- serve_local.py
+++ serve_local.py
@@ -6,9 +6,18 @@ import argparse
 QUARTZ_DIR = "quartz"
 
 
-def main(fast_start: bool):
+def main(fast_start: bool, fast_setup: bool):
+    if fast_start and fast_setup:
+        setup()
+        start()
+        return
+
     if fast_start:
         start()
+        return
+    
+    if fast_setup:
+        setup()
         return
 
     display_info()
@@ -16,6 +25,8 @@ def main(fast_start: bool):
     
     if inp == "1":
         start()
+    elif inp == "2":
+        setup()
 
 
 def display_info():
@@ -25,20 +36,24 @@ def display_info():
 
     print("Требования перед запуском (нужно сделать 1 раз):")
     print("1. Установить Node и npm")
-    print("2. Находясь в папке quartz, выполнить следующее:")
+    print("2. Находясь в папке quartz, выполнить следующее: (или пункт Setup)")
     print("npm i\nnpm audit fix\n")
 
     print("После запуска скрипта будет удалено всё содержимое папки quartz/content")
     print("Эта папка используется лишь для билда, поэтому не стоит что-либо пытаться редактировать в ней.\n")
 
-    print("Выберите действие:\n 1. Запуск\n 9. Стоп")
+    print("Выберите действие:\n 1. Запуск\n 2. Setup\n 9. Стоп")
+
+def setup():
+    run("npm i")
+    run("npm audit fix")
 
 
 def start():
-    shutil.rmtree("quartz/content")
+    shutil.rmtree("quartz/content", ignore_errors=True)
     shutil.copytree("content", "quartz/content", dirs_exist_ok=True)
     run_simple("python transform_files.py")
-    run_no_pipe("npx quartz build --serve")
+    run("npx quartz build --serve")
 
 def run_simple(command: str):
     process_result = subprocess.run(
@@ -56,28 +71,8 @@ def run(command: str):
     process = subprocess.Popen(
         command,
         cwd=QUARTZ_DIR,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,
-        bufsize=1,
-        universal_newlines=True
-    )
-
-    for output in process.stdout:
-        print(output.strip())
-
-    process.wait()
-    if process.returncode != 0:
-        print(f"Команда завершилась с кодом ошибки: {process.returncode}")
-
-def run_no_pipe(command: str):
-    process = subprocess.Popen(
-        command,
-        cwd=QUARTZ_DIR,
         shell=True
     )
-
     
     try:
         process.wait()
@@ -89,8 +84,8 @@ def run_no_pipe(command: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Скрипт для локального запуска Quartz")
-    parser.add_argument("-f", "--fast", action=argparse.BooleanOptionalAction, help="Запуск без информации о скрипте")
+    parser.add_argument("-f", "--fast", action="store_true", help="Быстрый запуск")
+    parser.add_argument("-s", "--setup", action="store_true", help="Быстрый Setup")
     args = parser.parse_args()
-    fast_start = args.fast
 
-    main(fast_start if fast_start else False)
+    main(args.fast, args.setup)
